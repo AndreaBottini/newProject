c69581a454c5d51963462ba73cbe49f148d3784c
Andrea Bottini <andrea.bottini22@gmail.com>
2023-11-02 17:36:39 +0100

    creazione funzione per mostrare dati e stampare a video

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-    alert("link")
+    let urlApi = 'https://jsonplaceholder.typicode.com/users/1/posts'
+    fetch(urlApi)
+        .then((response) => response.json())
+        .then((data) => {
+            function insertDataTable() {
+                const table = $('#dati-tabella tbody')
+                data.forEach((x) => { 
+                    table.append(
+                        `<tr><td>${x.id}</td><td>${x.title}</td><td>${x.body}</td><td>${x.userId}</td></tr>`)
+                });
+            }
+            insertDataTable() 
+        }) 
 });
+
