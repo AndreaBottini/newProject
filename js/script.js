27a026d6969bea65952592a387de6aba1dbcc355
Andrea Bottini <andrea.bottini22@gmail.com>
2023-11-03 18:23:42 +0100

    inserimento filtro tabella e select

--- js/script.js
+++ js/script.js
@@ -1,16 +1,37 @@
 $(document).ready(function() {
-    let urlApi = 'https://jsonplaceholder.typicode.com/users/1/posts'
+    let urlApi = 'https://jsonplaceholder.typicode.com/users/1/posts';
+    let data; // Variabile per conservare i dati
+
+    // Funzione per popolare la tabella con i dati
+    function insertDataTable(dati) {
+        const table = $('#dati-tabella tbody');
+        table.empty(); // Cancella il contenuto attuale della tabella
+        dati.forEach((x) => { 
+            table.append(
+                `<tr><td>${x.id}</td><td>${x.title}</td><td>${x.body}</td><td>${x.userId}</td></tr>`
+            );
+        });
+    }
+
+    // Esegui la chiamata API e popola la tabella iniziale
     fetch(urlApi)
         .then((response) => response.json())
-        .then((data) => {
-            function insertDataTable() {
-                const table = $('#dati-tabella tbody')
-                data.forEach((x) => { 
-                    table.append(
-                        `<tr><td>${x.id}</td><td>${x.title}</td><td>${x.body}</td><td>${x.userId}</td></tr>`)
-                });
-            }
-            insertDataTable() 
-        }) 
+        .then((response) => {
+            data = response; // Conserva i dati nella variabile
+            insertDataTable(data);
+        });
+
+    // Gestore eventi per la select
+    $("#filter-select").on("change", function() {
+        const utenteSelezionato = $(this).val();
+        if (utenteSelezionato === "all") {
+            // Selezionato "Tutti", mostra tutti i dati
+            insertDataTable(data);
+        } else {
+            // Filtra i dati in base all'utente selezionato
+            const datiFiltrati = data.filter((dato) => dato.userId == utenteSelezionato);
+            insertDataTable(datiFiltrati);
+        }
+    });
 });
 
